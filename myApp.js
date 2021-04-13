bd2eb6be8289377105808680eb7b282b8bb3f5d0
Sabri0o <sabritrabelsi90@gmail.com>
2021-04-13 23:50:22 +0100

    chain a middleware

--- myApp.js
+++ myApp.js
@@ -20,6 +20,20 @@ app.get("/", function (req, res) {
   res.sendFile(path);
 });
 
+// In the route app.get('/now', ...) chain a middleware function and the final handler.
+// In the middleware function you should add the current time to the request object in the req.time key.
+// You can use new Date().toString(). In the handler, respond with a JSON object, taking the structure {time: req.time}.
+app.get(
+  "/now",
+  function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  },
+  function (req, res) {
+    res.json({ time: req.time });
+  }
+);
+
 //The endpoint /json should serve the json object {"message": "Hello json"}
 
 app.get("/json", function (req, res) {
