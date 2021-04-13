8ecae8096b627628130916949ca64d4a6e9d29a3
Sabri0o <sabritrabelsi90@gmail.com>
2021-04-13 15:43:47 +0100

    serving assets

--- myApp.js
+++ myApp.js
@@ -1,39 +1,13 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
+// sendFile method will set the appropriate headers to instruct your browser on how to handle the file you want to send
+var static = express.static('public')
 
+app.get('/',function(req,res){
+  let path = __dirname + '/views/index.html'
+  res.sendFile(path)
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+// serving static assets
+app.use('/public',static)
